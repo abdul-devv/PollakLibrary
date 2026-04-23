23fbc5e242fd8eb94eb59c9526536a80e5311a67
Mohammad <asim_ali@csu.fullerton.edu>
2026-04-22 17:06:15 -0700

    created a new folder called Dtos and added 3 files in it

---
+++ DTOs/Book/CreateBookDto.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PollakLibrary.Api.DTOs.Book;
+
+public class CreateBookDto
+{
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(300, ErrorMessage = "Title cannot exceed 300 characters.")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Author is required.")]
+    [StringLength(200, ErrorMessage = "Author cannot exceed 200 characters.")]
+    public string Author { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "ISBN is required.")]
+    [StringLength(20, ErrorMessage = "ISBN cannot exceed 20 characters.")]
+    public string ISBN { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue, ErrorMessage = "TotalCopies must be greater than 0.")]
+    public int TotalCopies { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "AvailableCopies must be 0 or greater.")]
+    public int AvailableCopies { get; set; }
+}
